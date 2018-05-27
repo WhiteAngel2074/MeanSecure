3ce8010610f8619c4726f7b9ee1e7115311b39b9
Hamdi Chebbi <hamdi.chebbi@outlook.fr>
2018-05-27 22:19:18 +0100

    SigniIN route

--- routes/api.js
+++ routes/api.js
@@ -40,10 +40,41 @@ router.post('/signup', (req, res) => {
       }
       res.json({
         success: true,
-        msg: 'Successful created new user'
+        msg: 'Successfull created new user'
       });
     });
   }
+});
+
+// SigIN
+router.post('/signin', (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, function (err, user) {
+    if (!user) {
+      res.status(401).send({
+        success: false,
+        msg: 'Authentification failed. User Not found'
+      });
+    } else {
+      // Check If password match :
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          var token = jwt.sign(user.toJson(), config.secret);
+          res.json({
+            success: true,
+            token: 'JWT' + token
+          });
+        } else {
+          res.status(401).send({
+            success: false,
+            msg: 'Authentification failed. Wrong password'
+          });
+        }
+      })
+
+    }
+  })
 });
 
 module.exports = router;
