baea816c0805b725b39a9626df72295e38224ef9
Hamdi Chebbi <hamdi.chebbi@outlook.fr>
2018-05-27 17:42:47 +0100

    Models
    User & Book Models

--- app.js
+++ app.js
@@ -11,13 +11,28 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var config = require('./config/database');
+var api = require('./routes/api');
+
+app.use(passport.initialize());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 'extended': 'false' }));
+app.use(bodyParser.urlencoded({
+  'extended': 'false'
+}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
+app.use('/api', api);
+
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(config.database, {
+    promiseLibrary: require('bluebird')
+  })
+  .then(() => console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
